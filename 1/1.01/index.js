f995956c4676284754c8526076ef867b286b8c95
Anas Fajoui <anasfajoui@gmail.com>
2025-07-06 16:09:44 +0100

    INIT!: a fresh start for the 2025 edition

--- 1/1.01/index.js
+++
@@ -1,7 +0,0 @@
-const getString = () => {
-    const randomstring = (Math.random() + 1).toString(36).substring(2);
-    console.log(new Date(), randomstring);
-    setTimeout(getString, 5000);
-}
-
-getString();
